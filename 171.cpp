c68b45fc7cf616f11dbef79999609e31b977f1f0
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-02-22 23:50:36 +0800

    Update 171.cpp

--- 171.cpp
+++ 171.cpp
@@ -1,15 +1,15 @@
 /*
 https://leetcode.com/problems/excel-sheet-column-number/submissions/
-https://leetcode.com/submissions/detail/646709873/
-Runtime: 6 ms, faster than 19.43% of C++ online submissions for Excel Sheet Column Number.
-Memory Usage: 5.9 MB, less than 67.10% of C++ online submissions for Excel Sheet Column Number.
+https://leetcode.com/submissions/detail/646815951/
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Excel Sheet Column Number.
+Memory Usage: 5.9 MB, less than 92.06% of C++ online submissions for Excel Sheet Column Number.
 */
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
         int total = 0;
         for (auto i: columnTitle) {
-            total = total*26 + (i-'A'+1);
+            total = (i-'A'+1) + total*26;
         }
         return total;
     }
