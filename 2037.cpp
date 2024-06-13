97f21509c81e5788b461c110363bef2fd067f034
Andy8787(布長) <andy8787main@gmail.com>
2024-06-13 10:19:41 +0800

    每日任務 很簡單

--- 2037.cpp
+++ 2037.cpp
@@ -1,18 +1,14 @@
-/*
-https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/submissions/
-https://leetcode.com/submissions/detail/643967623/
-Runtime: 16 ms, faster than 36.53% of C++ online submissions for Minimum Number of Moves to Seat Everyone.
-Memory Usage: 18.2 MB, less than 44.09% of C++ online submissions for Minimum Number of Moves to Seat Everyone.
-*/
+// https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
         sort(seats.begin(), seats.end());
         sort(students.begin(), students.end());
-        int total = 0;
-        for (int i = 0; i < seats.size(); i++) {
-            total += abs(students[i]-seats[i]);
+
+        int ans = 0;
+        for (int i = 0; i < seats.size(); i++){
+            ans += abs(students[i]-seats[i]);
         }
-        return total;
+        return ans;
     }
 };
