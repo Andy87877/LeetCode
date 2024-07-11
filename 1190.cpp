b4ef411840c8081e6af7c5cd841b6bf47d3a0bba
Andy8787(布長) <andy8787main@gmail.com>
2024-07-11 21:37:21 +0800

    Create 1190.cpp

---
+++ 1190.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<string> stk;
+        string str;
+        for (auto &ch : s) {
+            if (ch == '(') {
+                stk.push(str);
+                str = "";
+            } else if (ch == ')') {
+                reverse(str.begin(), str.end());
+                str = stk.top() + str;
+                stk.pop();
+            } else {
+                str.push_back(ch);
+            }
+        }
+        return str;
+    }
+};
+/*
+( 加
+) 合併並反轉
+*/
