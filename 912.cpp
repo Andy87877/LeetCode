e29c6a8cf5879b887da2dc482ae7ccf6424572d5
Andy8787(布長) <andy8787main@gmail.com>
2024-07-25 18:00:30 +0800

    Create 912.cpp

---
+++ 912.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/sort-an-array/
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
